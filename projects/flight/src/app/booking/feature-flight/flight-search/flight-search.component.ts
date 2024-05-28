8a3fed068bb7908bc07bb0b0e8e2ff4fad0538bf
mikezks <23002661+mikezks@users.noreply.github.com>
2024-05-28 17:50:45 +0200

    fix: remove previous demos

--- projects/flight/src/app/booking/feature-flight/flight-search/flight-search.component.ts
+++ projects/flight/src/app/booking/feature-flight/flight-search/flight-search.component.ts
@@ -1,11 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectorRef, Component, Injector, ViewContainerRef, computed, effect, inject, runInInjectionContext, signal, untracked } from '@angular/core';
-import { SIGNAL } from '@angular/core/primitives/signals';
+import { Component, computed, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { Store } from '@ngrx/store';
 import { Flight, FlightFilter, injectTicketsFacade } from '../../logic-flight';
 import { FlightCardComponent, FlightFilterComponent } from '../../ui-flight';
-import { FlightService } from './../../logic-flight/data-access/flight.service';
 
 
 @Component({
@@ -21,11 +18,6 @@ import { FlightService } from './../../logic-flight/data-access/flight.service';
 })
 export class FlightSearchComponent {
   private ticketsFacade = injectTicketsFacade();
-  private flightState = injectFlightState({ delayed: true });
-  private injector = inject(Injector);
-  private viewRef = inject(ViewContainerRef);
-  private cdRef = inject(ChangeDetectorRef);
-  // flightService = inject(FlightService) as unknown as FlightService[];
 
   protected filter = signal<FlightFilter>({
     from: 'London',
@@ -48,63 +40,8 @@ export class FlightSearchComponent {
   };
   protected flights$ = this.ticketsFacade.flights$;
 
-  constructor() {
-    effect(() => {
-      const filter = this.filter();
-      untracked(() => console.log(filter));
-    });
-
-    this.filter.update(
-      filter => ({
-        ...filter,
-        from: 'Barcelona'
-      })
-    );
-    console.log(this.filter().from);
-    this.filter.update(
-      filter => ({
-        ...filter,
-        from: 'Vienna'
-      })
-    );
-    console.log(this.filter().from);
-    this.filter.update(
-      filter => ({
-        ...filter,
-        from: 'Rome'
-      })
-    );
-    console.log(this.filter().from);
-    console.log(this.flightDetails[SIGNAL]);
-
-    /**
-     * 0
-     * even
-     * 1
-     * even
-     * odd
-     * 2
-     * odd
-     * even
-     * */
-
-    /* const ref = this.viewRef.createComponent(FlightCardComponent);
-    outputToObservable(ref.instance.delayTrigger).pipe().subscribe(console.log);
-    ref.setInput('item', initialFlight); */
-
-    this.cdRef.markForCheck();
-  }
 
   protected search(filter: FlightFilter): void {
-    // Would trigger an injection context error
-    // injectFlightState({ delayed: true });
-    // inject(FlightService, {});
-
-    runInInjectionContext(
-      this.injector,
-      () => injectFlightState({ delayed: true })
-    );
-
     this.filter.set(filter);
 
     if (!this.filter().from || !this.filter().to) {
@@ -131,13 +68,4 @@ export class FlightSearchComponent {
   protected reset(): void {
     this.ticketsFacade.reset();
   }
-}
-
-
-function injectFlightState(config: { delayed: boolean }) {
-  const store = inject(Store);
-  const flights = inject(FlightService).flights
-    .filter(flight => flight.delayed === config.delayed);
-
-  return { store, flights };
 }
